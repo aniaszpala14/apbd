65356e96e196278a12e351cb438ee08e329a56e6
s27542 <s27542@pjwstk.edu.pl>
2024-03-08 15:18:10 +0100

    dodano metode liczącą średnią

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -2,3 +2,15 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Zmiana 3!");
+
+static double GetAverage(int[] numbers)
+{
+    double result = 0.0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+
+    return result / numbers.Length;
+}
+
